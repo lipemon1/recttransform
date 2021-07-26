132e9089bbc9d6efa4edba9ee1f537466af5b7a0
lipemon1 <ramon_felipe@outlook.com>
2021-07-25 22:37:17 -0300

    Removing unecessary logs

--- Assets/Scripts/CustomRectTransform.cs
+++ Assets/Scripts/CustomRectTransform.cs
@@ -23,12 +23,8 @@ public static class CustomRectTransform
         public void SetActive(bool value)
         {
             if (_active == value)
-            {
-                Debug.Log($"State will not change because it's the same: Cur[{_active}] / New[{value}]");
                 return;
-            }
 
-            Debug.Log("Changing rect value and pos");
             _active = value;
             RectTransform.anchoredPosition = value ? RectTransform.anchoredPosition - DISABLE_POS : RectTransform.anchoredPosition + DISABLE_POS;
 
@@ -42,15 +38,9 @@ public static class CustomRectTransform
         int rectId = rectTransform.GetInstanceID();
 
         if (rectDic.TryGetValue(rectId, out CustomRect customRect))
-        {
-            Debug.Log("Custom Rect found.");
             customRect.SetActive(value);
-        }
         else
-        {
-            Debug.Log("New Custom Rect");
             rectDic.Add(rectId, new CustomRect(rectTransform, value));
-        }
     }
 
     public static void ToggleUIObject(this RectTransform rectTransform, bool visibility)
