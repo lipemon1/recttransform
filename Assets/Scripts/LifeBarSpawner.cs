9519eba87558f821ae922cdd748051f599b229c9
lipemon1 <ramon_felipe@outlook.com>
2021-07-25 22:36:39 -0300

    Adding tests scripts

---
+++ Assets/Scripts/LifeBarSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBarSpawner : MonoBehaviour
+{
+    public static LifeBarSpawner Instance { get; set; }
+
+    public Transform Holder;
+    public GameObject LifePrefab;
+
+    [Space]
+    public List<Transform> SpawnPosList = new List<Transform>();
+    private List<RectTransform> SpawnedLifeBars = new List<RectTransform>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+    public List<RectTransform> SpawnLifeBars(int amount)
+    {
+        SpawnedLifeBars.Clear();
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject lifebar = Instantiate(LifePrefab, GetTransformPos());
+            SpawnedLifeBars.Add(lifebar.GetComponent<RectTransform>());
+        }
+
+        return SpawnedLifeBars;
+    }
+
+    private Transform GetTransformPos()
+    {
+        return SpawnPosList[Random.Range(0, SpawnPosList.Count - 1)];
+    }
+}
--- Assets/Scripts/RectTest.cs
+++ Assets/Scripts/RectTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class RectTest : MonoBehaviour
 {
@@ -11,5 +12,59 @@ public class RectTest : MonoBehaviour
     public void ToggleObject()
     {
         Target.ToggleUIObject(Value);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            RunTest();
+    }
+
+    [ContextMenu("Run Tests")]
+    public void RunTest()
+    {
+        Debug.Log("Starting tests");
+        Profiler.BeginSample("Test");
+        SpawnAndToggle(1);
+        SpawnAndToggle(10);
+        SpawnAndToggle(100);
+        SpawnAndToggle(1000);
+        Profiler.EndSample();
+    }
+
+    public void SpawnAndToggle(int amount) 
+    {
+        Profiler.BeginSample($"Spawning {amount}");
+        List<RectTransform> lifebarsToActive = LifeBarSpawner.Instance.SpawnLifeBars(amount);
+        Profiler.EndSample();
+
+
+        Profiler.BeginSample($"Simple Toggle {amount}");
+
+        Profiler.BeginSample($"Simple Toggle - Desactivate {amount}");
+        foreach (RectTransform lifebar in lifebarsToActive)
+            lifebar.gameObject.SetActive(false);
+        Profiler.EndSample();
+
+        Profiler.BeginSample($"Simple Toggle - Activate {amount}");
+        foreach (RectTransform lifebar in lifebarsToActive)
+            lifebar.gameObject.SetActive(true);
+        Profiler.EndSample();
+
+        Profiler.EndSample();
+
+
+        Profiler.BeginSample($"New Toggle {amount}");
+        Profiler.BeginSample($"New Toggle - Desactivate {amount}");
+        foreach (RectTransform lifebar in lifebarsToActive)
+            lifebar.ToggleUIObject(false);
+        Profiler.EndSample();
+
+        Profiler.BeginSample($"New Toggle - Activate {amount}");
+        foreach (RectTransform lifebar in lifebarsToActive)
+            lifebar.ToggleUIObject(true);
+        Profiler.EndSample();
+
+        Profiler.EndSample();
     }
 }
